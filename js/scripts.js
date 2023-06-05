69aead2db68b18bc2a04da460b6e88d23106a9dc
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-04 17:32:49 -0700

    add function to get answers from HTML via querySelectors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function hideAnswers(){
+  document.getElementById("c-sharp").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("react").setAttribute("class", "hidden");
+}
+
+function submitAnswers(e){
+  e.preventDefault();
+  hideAnswers();
+  let inputTypeOfEnviornment = document.querySelector("input[name='type-of-enviornment']:checked").value
+  let inputDifficulty = document.querySellector("input[name='difficulty':]:checked").value
+  let inputReading = document.querySelector("input[name='reading']:checked").value
+  let inputAnimal = document.querySelector("input[name='animal']checked").value
+  let inputSearch = document.querySelector("input[name='search']checked").value
+}
+
 function darkMode() {
   let element = document.body;
   element.classList.toggle("dark-mode");
